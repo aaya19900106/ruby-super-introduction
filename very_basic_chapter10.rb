9dc39e46845e5be5c14e57f68cdfad18655469d7
aaya19900106 <katsuya@startup-technology.com>
2021-08-18 21:32:54 +0900

    aaa

--- very_basic_chapter10.rb
+++ very_basic_chapter10.rb
@@ -1,9 +1,8 @@
 
-#10-1
-
 
 
 require "net/http"
 require "uri"
-uri = URI.parse("http://localhost:4567/drink")
-p Net::HTTP.get(uri)
+
+uri = URI.parse("http://localhost:4567/hi")
+puts Net::HTTP.get(uri) #=> "hi!"
